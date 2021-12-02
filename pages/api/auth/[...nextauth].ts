82160461953f59ba1e38221cc50dc89260881a29
korzewski <korzewski@gmail.com>
2021-12-02 16:14:04 +0100

    add todo comments

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
+// TODO: Add database with prisma
+// TODO: Deploy to vercel & test with supabase
+
 export default NextAuth({
   providers: [
     GoogleProvider({
