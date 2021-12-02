a9ec34061fd289c3638779081e7be4f120507375
korzewski <korzewski@gmail.com>
2021-12-02 17:27:25 +0100

    add db columns for auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,15 @@
 import NextAuth from 'next-auth'
+import { PrismaClient } from '.prisma/client'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import GoogleProvider from 'next-auth/providers/google'
 
 // TODO: Add database with prisma
 // TODO: Deploy to vercel & test with supabase
 
+const prisma = new PrismaClient()
+
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
